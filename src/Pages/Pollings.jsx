a78a823e6c1bed17a7dfbfcd272a0c426844c921
rezaawp <rezaaawp@gmail.com>
2023-02-22 08:37:07 +0700

    update deadline

--- src/Pages/Pollings.jsx
+++ src/Pages/Pollings.jsx
@@ -6,13 +6,18 @@ import Layout from "../Layouts/Layout";
 import { User } from "../Stores/SessionStore";
 
 const Pollings = () => {
-  const Session = useContext(User);
   const [pollings, setPollings] = useState([]);
+  const [noPolling, setNoPolling] = useState(false);
 
   const getAllPoll = () => {
     polling
       .index()
-      .then((res) => setPollings(res.data))
+      .then((res) => {
+        setPollings(res.data);
+        if (res.data.length == 0 || res.data.length < 1) {
+          setNoPolling(true);
+        }
+      })
       .catch((e) => console.log(e));
   };
 
@@ -23,56 +28,71 @@ const Pollings = () => {
   return (
     <>
       <Layout>
-        <div className="container-fluid">
-          <div className="row">
-            {pollings?.map((polling, i) => {
-              return (
-                <div className="col-md-3 mb-4" key={i}>
-                  <div className="card">
-                    <div
-                      align="center"
-                      className="text-center card-header fs-6 d-flex fw-bold justify-content-center align-items-start"
-                      style={{ height: "150px" }}
-                    >
-                      <div className="container-fluid p-0">
-                        <div className="row">
-                          <img
-                            className="rounded"
-                            src={`http://localhost:8000/${polling?.thumbnail}`}
-                            height="80"
-                            alt=""
-                            style={{ objectFit: "cover" }}
-                          />{" "}
-                        </div>
-                        <div className="row">
-                          <span className="text-center mt-2">
-                            {polling?.question}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="card-body">
-                      <small>
-                        - {polling?.votes_count} orang sudah menjawab voting ini
-                      </small>
-                      <br />
-                      <small>
-                        - voting ini memiliki {polling?.choises_count} pilihan
-                      </small>
-                      <br />
-                      <div className="container-fluid mt-2 d-flex justify-content-end p-0">
-                        <Link
-                          to={`/polling/${polling?.id}`}
-                          className="fw-bold mt-2 btn btn-primary btn-sm"
-                        >
-                          VOTE
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
+        <div className="container-fluid" style={{ height: "100%" }}>
+          <div className="row" style={{ height: "100%" }}>
+            {noPolling ? (
+              <>
+                <div
+                  className="container-fluid d-flex align-items-center justify-content-center"
+                  style={{ height: "100%" }}
+                >
+                  <h5>Data Polling Masih Belum Ada</h5>
                 </div>
-              );
-            })}
+              </>
+            ) : (
+              <>
+                {pollings?.map((polling, i) => {
+                  return (
+                    <div className="col-md-3 mb-4" key={i}>
+                      <div className="card">
+                        <div
+                          align="center"
+                          className="text-center card-header fs-6 d-flex fw-bold justify-content-center align-items-start"
+                          style={{ height: "150px" }}
+                        >
+                          <div className="container-fluid p-0">
+                            <div className="row">
+                              <img
+                                className="rounded"
+                                src={`http://localhost:8000/${polling?.thumbnail}`}
+                                height="80"
+                                alt=""
+                                style={{ objectFit: "cover" }}
+                              />{" "}
+                            </div>
+                            <div className="row">
+                              <span className="text-center mt-2">
+                                {polling?.question}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="card-body">
+                          <small>
+                            - {polling?.votes_count} orang sudah menjawab voting
+                            ini
+                          </small>
+                          <br />
+                          <small>
+                            - voting ini memiliki {polling?.choises_count}{" "}
+                            pilihan
+                          </small>
+                          <br />
+                          <div className="container-fluid mt-2 d-flex justify-content-end p-0">
+                            <Link
+                              to={`/polling/${polling?.id}`}
+                              className="fw-bold mt-2 btn btn-primary btn-sm"
+                            >
+                              VOTE
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
           </div>
         </div>
       </Layout>
